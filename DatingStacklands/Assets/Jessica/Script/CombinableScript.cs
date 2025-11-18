7a775adda988db23f3e0ebb25914fa801ab169ed
DyingRavioli <jyl9645@nyu.edu>
2025-11-17 23:23:18 -0500

    cards now combine to create new colors

--- DatingStacklands/Assets/Jessica/Script/CombinableScript.cs
+++ DatingStacklands/Assets/Jessica/Script/CombinableScript.cs
@@ -1,16 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class CombinableScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private float timer = 2;
+    private GameObject result;
+
+    public GameObject orange;
+    public GameObject purple;
+    public GameObject green;
+
+    public enum Colors
     {
-        
+        red,
+        blue,
+        yellow
     }
 
-    // Update is called once per frame
+    public Colors card_color;
+
     void Update()
     {
-        
+
+        if (transform.childCount != 0)
+        {
+            Colors childcolor = transform.GetChild(0).GetComponent<CombinableScript>().card_color;
+
+            switch (card_color)
+            {
+                case Colors.red:
+                    if (childcolor == Colors.blue)
+                    {
+                        result = purple;
+                    }
+                    else if (childcolor == Colors.yellow)
+                    {
+                        result = orange;
+                    }
+                    break;
+
+                case Colors.blue:
+                    if (childcolor == Colors.red)
+                    {
+                        result = purple;
+                    }
+                    else if (childcolor == Colors.yellow)
+                    {
+                        result = green;
+                    }
+                    break;
+
+                case Colors.yellow:
+                    if (childcolor == Colors.blue)
+                    {
+                        result = green;
+                    }
+                    else if (childcolor == Colors.red)
+                    {
+                        result = orange;
+                    }
+                    break;    
+            }
+
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                Instantiate(result);
+                Destroy(gameObject);
+            }  
+
+        }
+
+        else
+        {
+            timer = 2;
+        }
+
     }
+    
 }
