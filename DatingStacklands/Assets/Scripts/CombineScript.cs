3cfe62ca4034b0cafa88214cffc64665335bd6e2
aaltttf4 <jh9335@nyu.edu>
2025-11-23 23:28:35 -0500

    combine

---
+++ DatingStacklands/Assets/Scripts/CombineScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CombineScript : MonoBehaviour
+{
+    private float timer = 1f;
+    public GameObject gameManager;
+
+    public DialogueNode a;
+    public GameObject dateCard;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.childCount != 0)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                GameObject card = transform.GetChild(0).gameObject;
+                if (card == dateCard)
+                {
+                       gameManager.GetComponent<DialogueManager>().InitiateDialogue(a);
+                }
+            }
+        }
+    }
+}
